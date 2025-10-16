004ae5e48c69cebda4926f84744578d1367e827b
Surendhar A <surendharfx@gmail.com>
2025-10-16 12:12:10 +0530

    HCPTA

--- ec2-module.tf
+++ ec2-module.tf
@@ -4,3 +4,5 @@ module "ec2" {
 }
 
 // we need to run terraform init to download plugin and module configuration.
+// Not all the modules will create resources directly, some modules need some mandatory input. without that input module will not create any resources and it will throw error.
+// We can pass that input inside the module block {}
